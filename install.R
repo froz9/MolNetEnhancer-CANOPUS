7af8c095ad640399b5f47405aac1ce32503b9212
Alan Hernandez <f9.alan@gmail.com>
2026-03-01 11:10:07 -0600

    Create install.R

---
+++ install.R
@@ -0,0 +1,15 @@
+# install.R
+# Script to install all required dependencies for MolNetConsensus
+
+required_packages <- c("shiny", "igraph", "dplyr", "httr", "jsonlite", "DT")
+
+# Identify packages that are not yet installed
+new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+
+# Install missing packages
+if(length(new_packages)) {
+  message("Installing missing packages: ", paste(new_packages, collapse = ", "))
+  install.packages(new_packages, repos = "http://cran.us.r-project.org")
+} else {
+  message("All required packages are already installed. You are ready to run the app!")
+}
